86f62986a7bc0eb60615c012dc3d6a16e382e588
Dok4440 <dokimakimaki@gmail.com>
2021-01-14 20:07:06 +0100

    Trying a preloaded database for RawScripts

--- TCURawScripts/TCU_installer_latest.sh
+++ TCURawScripts/TCU_installer_latest.sh
@@ -52,7 +52,7 @@ echo "Downloading TCU dependencies"
 echo ""
 echo "This process usually takes about 10 seconds."
 sleep 5s
-cd "$root/$tempdir/TCUBetaBot" || echo "what?"
+cd "$root/$tempdir/TCUBetaBot" || return;
 dotnet restore
 clear
 echo "TCU Dependecies are downloaded"
