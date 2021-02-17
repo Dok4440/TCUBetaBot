3fbb833101116e050211debffd63701766f1cfca
Dok4440 <dokimakimaki@gmail.com>
2021-02-17 16:34:47 +0100

    Finished the .die command

--- NadekoBot.Core/Modules/Administration/SelfCommands.cs
+++ NadekoBot.Core/Modules/Administration/SelfCommands.cs
@@ -298,11 +298,17 @@ namespace NadekoBot.Modules.Administration
 
             [NadekoCommand, Usage, Description, Aliases]
             [OwnerOnly]
-            public async Task Die([Leftover] string msg = "")
+            public async Task Die([Leftover] string msg = null)
             {
                 try
                 {
-                    await ReplyConfirmLocalizedAsync("shutting_down", msg.ToString()).ConfigureAwait(false); // fixed error on shutdown without Leftover param
+                    if (msg == null)
+                    {
+                        string msg_no_param = "Not pushing to GitLab, `git stash`.";
+                        await ConfirmLocalizedAsync("shutting_down_no_param", msg_no_param.ToString()).ConfigureAwait(false); // stash command?
+                        return;
+                    }
+                    await ConfirmLocalizedAsync("shutting_down", msg.ToString()).ConfigureAwait(false);
                 }
                 catch
                 {
